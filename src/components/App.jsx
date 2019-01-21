d514417833c5d1e17c41f4892134914f1175f5cb
Kenneth Trott <kentrottjr@gmail.com>
2019-01-21 13:49:01 -0800

    Add form display

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
 
-//es6 instantiation
-var React = require('react');
+// var React = require('react');
 // var ReactDOM = require('react-dom');
+//es6 instantiation
+import React from 'react';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-    };
+    this.state = {name: '',
+                 message: ''};
+
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeMessage = this.handleChangeMessage.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleChangeName(event) {
+    this.setState({name: event.target.value});
+  }
+  
+  handleChangeMessage(event) {
+    this.setState({message: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name & message was submitted: ' + this.state.name + ': ' + this.state.message);
+    // event.preventDefault();
+  }
+
   render() {
-    return <h1>It's Working! {this.props.name}</h1>;
+    return (
+     <div>
+      <h1>Server Response:</h1>
+      <h1>Response Goes Here!</h1>
+
+     <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.name} onChange={this.handleChangeName} />
+          Message:
+          <input type="text" value={this.state.message} onChange={this.handleChangeMessage} />
+        </label>
+        <input type="submit" value="Send Message" />
+      </form>
+     </div>
+    );
   }
 }
 
